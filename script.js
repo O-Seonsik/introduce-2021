40dc86f6665be0f38b6915786d47176534c84df8
o-seonsik <dhtjstlr777@gmail.com>
2021-01-02 02:23:32 +0900

    Desktop release

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ let choreographer = new Choreographer({
         // first message
         {range: [-1, height], selector: '#box', type: 'scale', style: 'opacity', from: 0.5, to: 0},
         {range: [height, height*1.5], selector: '#eng', type: 'scale', style: 'opacity', from: 1, to: 0},
-        {range: [height/2, height*2.5], selector: '#eng', type: 'scale', style: 'width', from: 500, to: 0, unit: "px" }
+        {range: [height/2, height*2.5], selector: '#eng', type: 'scale', style: 'width', from: 500, to: 0, unit: "px" },
+
+        // mail
+        {range: [height*3.7, height*4], selector: '#mail', type: 'scale', style: 'transform:rotate', from: 0, to: -90, unit: 'deg'},
+        {range: [height*3.7, height*4], selector: '#mail', type: 'scale', style: 'top', from: 10, to: 80, unit: 'px'},
+        {range: [height*3.7, height*4], selector: '#mail', type: 'scale', style: 'right', from: 10, to: -50, unit: 'px'}
     ]
 })
 window.addEventListener('scroll', function() {
