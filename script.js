673a6f1676819c438a19ea2dbd594d70065cd871
o-seonsik <dhtjstlr777@gmail.com>
2021-01-02 01:22:41 +0900

    Add project, footer

--- script.js
+++ script.js
@@ -2,71 +2,18 @@ const height = window.innerHeight
 AOS.init();
 let choreographer = new Choreographer({
     animations: [
-        {
-            range: [window.innerHeight*2.5, height*3.2],
-            selector: '#kor',
-            type: 'scale',
-            style: 'opacity',
-            from: 1,
-            to: 0
-        },
-        {
-            range: [-1, height],
-            selector: '#box',
-            type: 'scale',
-            style: 'opacity',
-            from: 0.5,
-            to: 0
-        },{
-            range: [height, height*1.5],
-            selector: '#eng',
-            type: 'scale',
-            style: 'opacity',
-            from: 1,
-            to: 0
-        },{
-            range: [height/2, height*1.5],
-            selector: '#eng',
-            type: 'scale',
-            style: 'width',
-            from: 500,
-            to: 250,
-            unit: "px"
-        },{
-            range: [height*1.5, height*2.5],
-            selector: '#kor',
-            type: 'scale',
-            style: 'opacity',
-            from: 0,
-            to: 1
-        },{
-            range: [height*1.5, height*2.5],
-            selector: '#kor',
-            type: 'scale',
-            style: 'padding-top',
-            from: 200,
-            to: 0,
-            unit: "px"
-        },{
-            range: [-1, height],
-            selector: '#mail',
-            type: 'scale',
-            style: 'transform',
-            from: 90,
-            to: 30,
-            unit: "deg"
-        },{
-            range: [height*3.2, height*3.2],
-            selector: '#kor',
-            type: 'scale',
-            style: 'opacity',
-            from: 0,
-            to: 0
-        }
+        // second message
+        {range: [height*3.3, height*3.3], selector: '#kor', type: 'scale', style: 'width', from: 250, to: 0, unit: 'px'},
+        {range: [height*2.7, height*3.3], selector: '#kor', type: 'scale', style: 'opacity', from: 1, to: 0},
+        {range: [height*1.5, height*2.5], selector: '#kor', type: 'scale', style: 'opacity', from: 0, to: 1},
+        {range: [height*1.5, height*2.5], selector: '#kor', type: 'scale', style: 'padding-top', from: 200, to: 0, unit: "px"},
+
+        // first message
+        {range: [-1, height], selector: '#box', type: 'scale', style: 'opacity', from: 0.5, to: 0},
+        {range: [height, height*1.5], selector: '#eng', type: 'scale', style: 'opacity', from: 1, to: 0},
+        {range: [height/2, height*2.5], selector: '#eng', type: 'scale', style: 'width', from: 500, to: 0, unit: "px" }
     ]
 })
-
-
 window.addEventListener('scroll', function() {
     choreographer.runAnimationsAt(window.pageYOffset)
 })
